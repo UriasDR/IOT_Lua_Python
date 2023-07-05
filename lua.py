205fdc245236fb4dbd74c11ffccc49c49b8883d5
UriasDR <129330378+UriasDR@users.noreply.github.com>
2023-07-05 04:10:37 -0300

    Add files via upload

--- lua.py
+++ lua.py
@@ -3,45 +3,39 @@ import socket
 postes = 4
 
 server_ip = '127.0.0.1'
-server_ports = [12345] #, 12346, 12347, 12348
+server_ports = [12345, 12346, 12347, 12348] #, 12346, 12347, 12348
 
 luas = "crescente3, minguante3, quarto_crescente6, quarto_minguante6, nova0, grande_crescente12, grande_minguante12, cheia15"
+luasVet = ["crescente", "minguante", "quarto_crescente", "quarto_minguante", "nova", "grande_crescente", "grande_minguante", "cheia"]
 nuvens = "nublado6, limpo15, chuva3 ou temporal0"
 
-luas = [
-    {
-        name: "crescente",
-        value: 3
-    },
+nuvensVet = ["nublado", "limpo", "chuva" , "tempestade"]
 
-]
+lua = ""
+while not lua:
+        lua = input("Qual a fase da lua? (Crescente, Minguante, Quarto_crescente, Quarto_minguante, Nova, Grande_Crescente, Grande_minguante e Cheia): ")
+        if lua.lower() not in luasVet:
+                print("Entrada Inválida")
+                lua = ""
+        nuvem = ""
+        while not nuvem:
+            nuvem = input("Como está o céu? (Nublado, Limpo, Chuva ou Tempestade): ")
+            if nuvem.lower() not in nuvensVet:
+                print("Entrada Inválida")
+                nuvem = ""
 
 try:
     for port in server_ports:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        lua = ""
-        while not lua:
-            lua = input("Qual a fase da lua? (Crescente, Minguante, Quarto_crescente, Quarto_minguante, Nova, Grande_Crescente, Grande_minguante e Cheia): ")
-            if lua.lower() not in luas:
-                print("Entrada Inválida")
-                lua = ""
-
-        nuvem = ""
-        while not nuvem:
-            nuvem = input("Como está o céu? (Nublado, Limpo, Chuva ou Temporal): ")
-            if nuvem.lower() not in nuvens:
-                print("Entrada Inválida")
-                nuvem = ""
 
         client_socket.connect((server_ip, port))
 
-        message = lua + ";" + nuvem
-        client_socket.sendall(message.encode())
+        Lua_Nuvem = lua + ";" + nuvem
+        client_socket.sendall(Lua_Nuvem.encode())
 
-        modified_message = client_socket.recv(1024).decode()
-        lua, nuvem = modified_message.split(";")
-        print("String Modificada (Porta", port, "):", lua, nuvem)
+        message_recieved = client_socket.recv(1024).decode()
+        print(message_recieved)
 
 except ConnectionRefusedError:
     print("A conexão não foi efetuada, erro ao conectar ao servidor")
--- poste_1.py
+++ poste_1.py
@@ -17,13 +17,63 @@ try:
 
         request = client_socket.recv(1024).decode()
         lua, nuvem = request.split(";")
-
+        lua = lua.lower();
+        nuvem = nuvem.lower();
+        
         soma = 0;
 
+        print(lua, nuvem)
+        luaValue = 0;
+        nuvemValue = 0;
+        Visibility = 0;
         
-        modified_message = request.upper()
+        Output = "";
+        
+        # crescente 3, minguante 3, quarto_crescente 6, quarto_minguante 6, nova 0, grande_crescente 12, grande_minguante 12, cheia 15
+        
+        # "nublado 6, limpo 15, chuva 3 ou tempestade 0"
+        
+        if(lua == "crescente" or  lua == "minguante"):
+            luaValue = 3;
+        elif(lua == "quarto_crescente" or lua == "quarto_minguante"):
+            luaValue = 6;
+        elif(lua == "nova"):
+            luaValue = 0;
+        elif(lua == "grande_crescente" or lua == "grande_minguante"):
+            luaValue = 12;
+        elif(lua == "cheia"):
+            luaValue = 15;
+            
+        if(nuvem == "nublado"):
+            nuvemValue = 6;
+        elif(nuvem == "limpo"):
+            nuvemValue = 15;
+        elif(nuvem == "chuva"):
+            nuvemValue = 3;
+        elif(nuvem == "tempestade"):
+            nuvemValue = 0;
+        
+        Visibility = luaValue + nuvemValue;
+        print(luaValue, nuvemValue)
+        
+        Energy = ""
+        for i in range(Visibility // 3):
+            Energy += "🔆"
+            
+        if(Energy == ""): Energy += "🎇"
 
-        client_socket.send(modified_message.encode())
+        if Visibility >= 0 and Visibility < 10:
+            Output = "Baixa Visibilidade, Energia poupada: " + Energy + "\nConsumo de energia elevado."
+        elif Visibility >= 10 and Visibility <= 21:
+            Output = "Visibilidade Média, Energia poupada: " + Energy + "\nConsumo de energia moderado."
+        elif Visibility > 21 and Visibility <= 30:
+            Output = "Visibilidade Alta, Energia poupada: " + Energy + "\nConsumo de energia reduzido."
+        else:
+            Output = "Erro"
+        
+        message = Output
+
+        client_socket.send(message.encode())
 
         client_socket.close()
 
